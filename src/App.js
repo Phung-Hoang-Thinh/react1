a04b59cdf1817469d5f25422672db5c2ebdc0c8a
Phung-Hoang-Thinh <122436388+Phung-Hoang-Thinh@users.noreply.github.com>
2024-02-04 19:27:58 +0700

    react1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+import BaiTapLayOutBS from './BaiTapLayoutBS/BaiTapLayOutBS'
+import BaiTapThuKinh from './BaiTapThuKinh/BaiTapThuKinh';
+
+
+
+function App() {
+  return (
+
+    <BaiTapLayOutBS />
+    // <BaiTapThuKinh />
+
+  );
+}
+
+export default App;
---
+++ src/BaiTapLayoutBS/Footer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="bg-dark text-white text-center mt-3 py-5">
+        <h1 className="m-0">Footer</h1>
+        <span>Phùng Hoàng Thịnh - DN10 - 2024 - React</span>
+      </div>
+    );
+  }
+}
